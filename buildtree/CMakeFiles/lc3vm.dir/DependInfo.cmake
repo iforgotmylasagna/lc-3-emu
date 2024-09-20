495cec86772fc45e9703fbbb1ac7ad524e7feb65
Kamran <kamranafsharian@protonmail.com>
2024-09-20 01:05:03 -0400

    delete

---
+++ buildtree/CMakeFiles/lc3vm.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kami/playground/c++/lc-3-emu/src/file_reader.cpp" "CMakeFiles/lc3vm.dir/src/file_reader.cpp.o" "gcc" "CMakeFiles/lc3vm.dir/src/file_reader.cpp.o.d"
+  "/home/kami/playground/c++/lc-3-emu/src/main.cpp" "CMakeFiles/lc3vm.dir/src/main.cpp.o" "gcc" "CMakeFiles/lc3vm.dir/src/main.cpp.o.d"
+  "/home/kami/playground/c++/lc-3-emu/src/pixbuff.cpp" "CMakeFiles/lc3vm.dir/src/pixbuff.cpp.o" "gcc" "CMakeFiles/lc3vm.dir/src/pixbuff.cpp.o.d"
+  "/home/kami/playground/c++/lc-3-emu/src/term_format_linux.cpp" "CMakeFiles/lc3vm.dir/src/term_format_linux.cpp.o" "gcc" "CMakeFiles/lc3vm.dir/src/term_format_linux.cpp.o.d"
+  "/home/kami/playground/c++/lc-3-emu/src/term_format_win.cpp" "CMakeFiles/lc3vm.dir/src/term_format_win.cpp.o" "gcc" "CMakeFiles/lc3vm.dir/src/term_format_win.cpp.o.d"
+  "/home/kami/playground/c++/lc-3-emu/src/vm.cpp" "CMakeFiles/lc3vm.dir/src/vm.cpp.o" "gcc" "CMakeFiles/lc3vm.dir/src/vm.cpp.o.d"
+  "/home/kami/playground/c++/lc-3-emu/src/vm_op_functions.cpp" "CMakeFiles/lc3vm.dir/src/vm_op_functions.cpp.o" "gcc" "CMakeFiles/lc3vm.dir/src/vm_op_functions.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ buildtree/CMakeFiles/lc3vm.dir/src/term_format_win.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/lc3vm.dir/src/term_format_win.cpp.o: \
+ /home/kami/playground/c++/lc-3-emu/src/term_format_win.cpp \
+ /usr/include/stdc-predef.h
