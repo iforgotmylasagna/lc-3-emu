9f7c5612331155aa3ff5b24e3a7bd7183981f76e
Kamran <kamranafsharian@protonmail.com>
2024-09-20 01:07:28 -0400

    delete

--- buildtree/CMakeFiles/lc3vm.dir/src/pixbuff.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/lc3vm.dir/src/pixbuff.cpp.o: \
- /home/kami/playground/c++/lc-3-emu/src/pixbuff.cpp \
- /usr/include/stdc-predef.h
--- buildtree/CMakeFiles/lc3vm.dir/src/term_format_win.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/lc3vm.dir/src/term_format_win.cpp.o: \
- /home/kami/playground/c++/lc-3-emu/src/term_format_win.cpp \
- /usr/include/stdc-predef.h
