6d3a4d50cfd413d2411ece313c5f9ba62e685a1f
Kamran <kamranafsharian@protonmail.com>
2024-09-20 20:11:44 -0400

    main

--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 
 # How to build:
 
-    * install gcc
-      *On Fedora: sudo dnf install gcc
-    * install cmake
-      *On Fedora: sudo dnf install cmake
-    * chmod +x compile.sh
-    * ./compile
+    install gcc
+      On Fedora: sudo dnf install gcc
+
+    install cmake
+      On Fedora: sudo dnf install cmake
+
+    chmod +x compile.sh
+
+    ./compile
 
 # How to run:
 
-    * ./bin/run <file_path>
+     ./bin/run <file_path>
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include "lc3vm/vm.hpp"
 #include <iostream>
+#include <string>
 //#include <SDL.h> //use this later for displaying screen and maybe input
-
 using namespace lc3vm;
 
-int main(){
+void runvm(const std::string& path){
 vm test{};
-int i=test.load_program("bin/2048.obj");
+int i=test.load_program(path);
 if(i==0){
   std::cout << "PROGRAM LOAD SUCCESS"<<std::endl;
  }
@@ -14,8 +14,13 @@ std::cout << "RUNNING..."<<std::endl;
 int j=test.run(mode::normal);
 if(j<0){
   std::cout << "VM ERROR"<<std::endl;
-  return 0;
  }
 std::cout << "RUN SUCCESS" <<std::endl;
+}
+
+
+
+int main(){
+runvm("bin/2048.obj");
 return 0;
 }
