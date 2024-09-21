6a4344236c686b0a06738b8d3d4e68e537ff8d87
Kamran <kamranafsharian@protonmail.com>
2024-09-20 20:26:48 -0400

    main

--- README.md
+++ README.md
@@ -12,6 +12,12 @@
 
     ./compile
 
-# How to run:
+# How to run program:
 
-     ./bin/run <file_path>
+    ./bin/run <file_path>
+
+# Some programs to test out:
+
+    https://github.com/junybike/LC3-My-Own-Virtual-Machine/blob/main/rogue.obj
+    
+    https://github.com/rpendleton/lc3-2048
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include "lc3vm/vm.hpp"
 #include <iostream>
 #include <string>
+#include <filesystem>
 //#include <SDL.h> //use this later for displaying screen and maybe input
 using namespace lc3vm;
 
 void runvm(const std::string& path){
 vm test{};
-int i=test.load_program(path);
-if(i==0){
+if(test.load_program(path)){
   std::cout << "PROGRAM LOAD SUCCESS"<<std::endl;
  }
 std::cout << "RUNNING..."<<std::endl;
-int j=test.run(mode::normal);
-if(j<0){
+if(test.run(mode::normal)){
   std::cout << "VM ERROR"<<std::endl;
  }
 std::cout << "RUN SUCCESS" <<std::endl;
@@ -20,7 +19,21 @@ std::cout << "RUN SUCCESS" <<std::endl;
 
 
 
-int main(){
-runvm("bin/2048.obj");
-return 0;
+int main(int argc, char* argv[]){
+if (argc < 2){
+  std::cout << "No file: Use: " << argv[0] << " <file-path>\n";
+  return -1;
+}
+
+std::string testpath = argv[1];
+std::filesystem::path path = testpath;
+
+
+if(std::filesystem::exists(testpath)){
+  runvm(testpath);
+  return 0;
+ }
+
+std::cout << "Invalid File Path" << std::endl;
+return -1;
 }
